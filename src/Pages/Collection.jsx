78a59abd661b790501d6b0f5813191c64a922066
Carol Wambui <ericrain717@gmail.com>
2026-02-22 23:49:26 +0300

    collection Page

--- src/Pages/Collection.jsx
+++ src/Pages/Collection.jsx
@@ -11,6 +11,7 @@ const Collection = () => {
   const [filterProducts, setFilterProducts] = useState([]);
   const [category, setCategory] = useState([]);
   const [subCategory, setSubCategory] = useState([]);
+  const [sortType,setSortType] =useState("relevant")
 
   // ---------- HELPERS ----------
   const normalize = (str) =>
@@ -56,14 +57,32 @@ const Collection = () => {
     setFilterProducts(productCopy);
   };
 
+  const sortProduct= ()=>{
+    let fpCopy= filterProducts.slice();
+    switch(sortType){
+      case "low-high":
+      setFilterProducts(fpCopy.sort((a,b)=>(a.price- b.price)));
+      break;
+
+      case "high-low":
+      setFilterProducts(fpCopy.sort((a,b)=>(b.price-a.price)));
+      break;
+      default:
+        applyFilter();
+        break;
+    }
+  }
   // ---------- EFFECTS ----------
-  useEffect(() => {
-    setFilterProducts(products);
-  }, [products]);
+ 
 
   useEffect(() => {
     applyFilter();
   }, [category, subCategory]);
+
+  useEffect(()=>{
+    sortProduct();
+
+  },[sortType])
 
   return (
     <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
@@ -135,10 +154,10 @@ const Collection = () => {
         <div className="flex justify-between text-base sm:text-2xl mb-4">
           <Title text1="ALL" text2="COLLECTIONS" />
 
-          <select className="border-2 border-gray-300 text-sm px-2">
-            <option>Sort by: Relevant</option>
-            <option>Sort by: Low to High</option>
-            <option>Sort by: High to Low</option>
+          <select onChange={(e)=>setSortType(e.target.value)}className="border-2 border-gray-300 text-sm px-2">
+            <option value="relevant">Sort by: Relevant</option>
+            <option value="low-high">Sort by: Low to High</option>
+            <option value="high-low">Sort by: High to Low</option>
           </select>
         </div>
 
